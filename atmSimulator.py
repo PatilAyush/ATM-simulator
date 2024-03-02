20bf95a52b5c72b1f913c9c72b620cb2f578c96e
PatilAyush <161905632+PatilAyush@users.noreply.github.com>
2024-03-02 15:18:01 -0500

    Creation of atmSimulator.py
    Edits:
    
      - Put in the loop
      - Started on deposit

---
+++ atmSimulator.py
@@ -0,0 +1,11 @@
+#ATM simulator
+amount = 0
+
+while True:
+  print("Welcome to the ATM\nWhat would you like to do today?\n  - Deposit\n  - Withdraw\n  - Balance\n  - Exit\n ")
+  choice = input("")
+  if choice.lower() == 'deposit':
+    amount = float(input("Enter the amount of money you are going to be depositing: ")
+    
+    
+  
