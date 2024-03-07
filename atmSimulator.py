2a171eb2b921d3ad66fea66b7fa18c36dbb90e78
PatilAyush <161905632+PatilAyush@users.noreply.github.com>
2024-03-06 22:33:49 -0500

    Adding other functions
    Balance, withdraw and exit are functional

--- atmSimulator.py
+++ atmSimulator.py
@@ -1,11 +1,20 @@
 #ATM simulator
 amount = 0
+balance = 0
 
 while True:
   print("Welcome to the ATM\nWhat would you like to do today?\n  - Deposit\n  - Withdraw\n  - Balance\n  - Exit\n ")
   choice = input("")
   if choice.lower() == 'deposit':
-    amount = float(input("Enter the amount of money you are going to be depositing: ")
-    
-    
-  
+    amount = float(input("Enter the amount of money you are going to be depositing: \n"))
+    balance = balance + amount
+    print("\nDone!\n")
+  elif choice.lower() == 'withdraw':
+    amount = float(input("Enter the amount of money you are going to be withdrawing: \n"))
+    balance = balance - amount
+    print("\nDone\n")
+  elif choice.lower() == 'balance':
+    print(balance,"\n")
+  elif choice.lower() == 'exit':
+    print("Thanks\n")
+    break
